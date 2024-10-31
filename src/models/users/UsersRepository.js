8a7d89c6ca383ad258fc2f1331d8cd187244f129
Julianeves01 <julianvs2007@gmail.com>
2024-10-31 17:18:47 -0300

    feat: create update user method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -24,6 +24,22 @@ class UsersRepository {
 
     return user;
     }
+
+    updateUser(id, name, email, password) {
+        const user = this.getUserById(id);
+
+        if (!user) {
+            return null;
+        }
+        
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        return user;
+
+    }
+
 }
 
 export default UsersRepository;
